c1c6f31624ef69ea95b4d74a3287c4a124dd0e8c
Sebastian Gierth <sebastian.gierth@xima.de>
2015-05-06 16:16:01 +0200

    Add a script to switch between configuration files of cores

---
+++ scripts/linux/switchCore.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+#
+# Crawl-Anywhere
+#
+# Author: Sebastian Gierh <sgi@xima.de>
+#
+# Core switch script
+#
+
+# Get the path of crawl anywhere root directory
+HOME=/opt/crawler
+SCRIPTS=$HOME/scripts
+
+# Init Variables
+. $SCRIPTS/init.inc.sh
+
+usage() {
+	echo "Usage : $SCRIPTS/switchCore.sh {CoresDirectory} {ScriptDirectory}"
+}
+
+if [ $# -lt 2 ]; then
+	usage
+	exit 0
+fi
+
+getPID() {
+	if [ -e "$1" ]; then
+		cat $1
+		return 0
+	fi
+	return 1
+}
+
+PID_INDEXER=$(getPID "$LOG_DIR/indexer.pid")
+PID_PIPELINE=$(getPID "$LOG_DIR/pipeline.pid")
+
+result=
+
+# Get next directory by current dirname
+# $1 dir to work in
+# $2 dirname inside $1
+nextDir() {
+	local curDir=0
+	local firstDir=
+
+	for x in $1*/; do
+		if [ -z $firstDir ]; then
+			firstDir=$x
+		fi
+		if [ -d $x ]; then
+			if [ $curDir -eq 1 ]; then
+				result=$x
+				return 0
+			fi
+			if [ "$x" == "$1$2/" ]; then
+				curDir=1
+			fi
+		fi
+	done
+
+	result=$firstDir
+	return 0
+}
+
+# stop/start indexer and pipeline with core configuration
+# $1 path to core
+# $2 path to scripts
+handleScript() {
+	if [ -z $1 ]; then
+		echo "Missing core!"
+		return 1
+	fi
+
+	# pipeline
+	sh "$2pipeline.sh" stop &> /dev/null
+	sleep 10
+	sh "$2pipeline.sh" start "$1config/pipeline/simplepipeline.xml" &> /dev/null
+	sleep 5
+	sh "$2pipeline.sh" status
+
+	# indexer
+	sh "$2indexer.sh" stop &> /dev/null
+	sleep 10
+	sh "$2indexer.sh" start "$1config/indexer/indexer.xml" &> /dev/null
+	sleep 5
+	sh "$2indexer.sh" status
+
+	return 0
+}
+
+### Main ###
+
+[[ -n $PID_PIPELINE ]] && PID=$PID_PIPELINE || PID=$PID_INDEXER
+
+# get current running core
+currentCore=
+if [ -n $PID ]; then
+	currentCore="$( ps -o args $PID | grep -oP '(?<=\s-p\s)\S*' | grep -oP "(?<=$1)[^/]+" )"
+fi
+if [ -z $currentCore ]; then
+	currentCore="$1./"
+fi
+
+# get next core
+nextDir "$1" "$currentCore"
+nextCore="$( echo $result | grep -oP "(?<=$1)[^/]+" )"
+
+# try switch
+echo "Trying to switch core from '$currentCore' to '$nextCore'"
+handleScript "$1$nextCore/" "$SCRIPTS"
+
+exit 0;
